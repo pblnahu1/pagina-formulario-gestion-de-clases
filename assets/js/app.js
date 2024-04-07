c9ab0052ff98b68fed5fe3abaefbf8e5cf42ecb2
Maxi <maxiakddd@gmail.com>
2024-04-07 13:13:02 -0300

    e

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 
-const d = document
-const $modal = d.querySelector(".modal") // Variable global
+const d = document;
+const $modal = d.querySelector(".modal"); // Variable global
+const $modal2 = d.querySelector(".modal2"); // Variable global
+const fondo_modal = document.querySelector(".modal__container");  
+const inputs_modal = document.querySelectorAll(".inputs-form-alta");
+
 
 export const fn_button_alta = (openModalAlta, closeModalAlta) => {
   openModalAlta.addEventListener("click", (e) => {
@@ -15,10 +19,11 @@ export const fn_button_alta = (openModalAlta, closeModalAlta) => {
       .catch(err => console.error(err))
     */
     $modal.classList.add("modal--show")
+    
   })
 
   closeModalAlta.addEventListener("click", (e) => {
-    e.preventDefault()
+    e.preventDefault();
     /*fetch('assets/templates/modal_alta.html')
       .then(response => response.text())
       .then(template => {
@@ -27,7 +32,13 @@ export const fn_button_alta = (openModalAlta, closeModalAlta) => {
       })
       .catch(err => console.error(err))
     */
-    $modal.classList.remove("modal--show")
+      $modal.classList.remove("modal--show");
+      const bad = document.querySelector(".bad"); 
+      fondo_modal.style.borderColor="white";
+      for (let i = 0; i < inputs_modal.length; i++) {
+        inputs_modal[i].style.borderColor = "white";
+      }      
+      bad.style.display="none";
   })
 }
 
@@ -36,10 +47,38 @@ export const fn_button_modificacion = (btnModificar) => btnModificar.addEventLis
   alert("Hiciste click en Modificar")
 })
 
-export const fn_button_baja = (btnBaja) => btnBaja.addEventListener("click", (e) => {
-  e.preventDefault()
-  alert("Hiciste click en Baja")
-})
+export const fn_button_baja = (openModalBaja, closeModalBaja) => {
+  openModalBaja.addEventListener("click", (e) => {
+    e.preventDefault()
+    /*fetch('assets/templates/modal_alta.html')
+      .then(response => response.text())
+      .then(template => {
+        console.log(template)
+        $modal.innerHTML = template
+        $modal.classList.add("modal--show")
+      })
+      .catch(err => console.error(err))
+    */
+    $modal2.classList.add("modal2--show")
+    
+  })
+
+
+  closeModalBaja.addEventListener("click", (e) => {
+    e.preventDefault();
+    /*fetch('assets/templates/modal_alta.html')
+      .then(response => response.text())
+      .then(template => {
+        console.log(template)
+        $modal.classList.remove("modal--show")
+      })
+      .catch(err => console.error(err))
+    */
+      $modal2.classList.remove("modal2--show");
+    
+  })
+}
+
 
 // Si no marqué ningun input checkbox, el botón de modificar debe estar inhabilitado
 // Si marqué varios input checkbox, el botón de modificar debe estar inhabilitado, ya que solo se puede modificar 1 solo a la vez
---
+++ assets/templates/modal_alta.php
@@ -0,0 +1,109 @@
+
+<section class="modal">
+    <div class="modal__container">
+        <div class="btn-cerrar-modal"> <a href="#" class="modal__close" title="Cerrar">X</a>  </div>
+    
+        <h2 class="modal__title">Alta de Clase</h2>
+    
+    <form method="post">
+
+        <label >
+            Materia
+            <select class="inputs-form-alta" name="materia">
+            <option value="" >Seleccione la materia</option>
+            <option value="1" >1.Análisis Matemático 1</option>
+            <option value="2" >2.Inglés 2</option>
+            <option value="3" >3.Prácticas Profesionalizantes 3</option>
+            </select>
+        </label>
+
+        <label >
+            Comision
+            <select class="inputs-form-alta" name="comision">
+                <option value="" >Seleccione la comision</option>
+                <option value="1.º, 1.ª" >1.º, 1.ª</option>
+                <option value="1.º, 2.ª" >1.º, 2.ª</option>
+                <option value="1.º, 3.ª" >1.º, 3.ª</option>
+            </select>  
+        </label>
+
+        <label >
+            Hora
+            <select class="inputs-form-alta" name="hora">
+                <option value="" >Seleccione hora</option>
+                <option value="8:00 a 10:00">8:00 a 10:00</option>
+                <option value="8:00 a 12:00" >8:00 a 12:00</option>
+                <option value="10:10 a 12:00" >10:10 a 12:00</option>
+                <option value="20:10 a 22:00" >20:10 a 22:00</option>
+                <option value="18:00 a 22:00" >18:00 a 22:00</option>
+                <option value="18:00 a 20:00" >18:00 a 20:00</option>
+            </select>      
+        </label>
+        
+        <label >
+            Fecha
+            <input class="inputs-form-alta" type="date" name="fecha" >
+        </label>
+
+        <textarea  name="temas" maxlength="50" rows="2" placeholder="Ingresá una tarea" class="textarea-form-class"></textarea>
+    
+        <textarea name="novedad" maxlength="50" rows="2" placeholder="¿Alguna Novedad?" class="textarea-form-class"></textarea>
+
+        <input type="submit" name="btn_Añadir" value="Añadir" class="btn_añadir">
+
+    </form>
+    <h3 class="bad">¡Por favor complete los campos indicados de manera correcta! </h3>
+
+    </div>
+</section>
+
+
+
+
+<?php
+
+if(isset($_POST['btn_Añadir'])){  
+if(strlen($_POST['materia']) >= 1 && strlen($_POST['comision']) > 0 &&  strlen($_POST['fecha']) > 0
+&& strlen($_POST['hora']) >0  &&  strlen($_POST['temas']) >= 0  && strlen($_POST['novedad']) >= 0)  { 
+    $id_profe = $_SESSION["id"];
+    $materia = trim($_POST['materia']);
+    $comision=trim($_POST['comision']);
+    $fecha_reg = trim($_POST['fecha']);
+    $hora = trim($_POST['hora']);
+    $temas= trim($_POST['temas']);
+    $novedad = trim($_POST['novedad']);
+    $consulta = "INSERT INTO clases(CODIGO_PROFESOR,CODIGO_MATERIA,COMISION,FECHA,HORA,TEMAS,NOVEDAD) VALUES 
+                                    ('$id_profe','$materia','$comision','$fecha_reg','$hora','$temas','$novedad')";  
+    $resultado= mysqli_query($conexionBD,$consulta);  
+    if($resultado){
+            header("Location:index.php");
+        ?>
+        <?php
+    }
+    else{
+        ?>
+        <h3 class="bad">¡Ups ha ocurrido un error! </h3>
+        <?php
+    }
+}
+else{
+    ?>
+    <script>
+        const $modal = document.querySelector(".modal");
+        const bad = document.querySelector(".bad") ;
+        const fondo_modal = document.querySelector(".modal__container");  
+        const inputs_modal = document.querySelectorAll(".inputs-form-alta");
+
+        $modal.classList.add("modal--show");
+        fondo_modal.style.borderColor="red";
+        for (let i = 0; i < inputs_modal.length; i++) {
+        inputs_modal[i].style.borderColor = "red";
+        }      
+        bad.style.display="block";
+    </script>
+
+    <?php
+    }
+}
+
+?>
